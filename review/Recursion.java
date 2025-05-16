cb99a5e8fb644eb5e0207bb6e1983b332f7c7aa7
dunggdinh <dung250205@gmail.com>
2025-05-16 09:16:33 +0700

    complete

---
+++ review/Recursion.java
@@ -0,0 +1,11 @@
+package review;
+
+public class Recursion {
+    public static long fac(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * fac(n - 1);
+        }
+    }
+}
---
+++ test/TestRecursion.java
@@ -0,0 +1,10 @@
+package test;
+
+import review.Recursion;
+
+public class TestRecursion {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(Recursion.fac(n));
+    }
+}
