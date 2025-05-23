7d30d411e5c8ca4f618164b9f43ad3e700b84ff0
dunggdinh <dung250205@gmail.com>
2025-05-23 08:43:31 +0700

    Add PlatformManager

---
+++ src/Classes/PlatformManager.java
@@ -0,0 +1,48 @@
+package Classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlatformManager {
+    private List<Platforms> platforms;
+
+    public PlatformManager() {
+        platforms = new ArrayList<>();
+    }
+
+    // CREATE
+    public void addPlatform(int x, int y, int width, int height) {
+        platforms.add(new Platforms(x, y, width, height));
+    }
+
+    // READ
+    public Platforms getPlatform(int index) {
+        if (index >= 0 && index < platforms.size()) {
+            return platforms.get(index);
+        }
+        return null;
+    }
+
+    public List<Platforms> getAllPlatforms() {
+        return platforms;
+    }
+
+    // UPDATE
+    public boolean updatePlatform(int index, int newX, int newY, int newWidth, int newHeight) {
+        Platforms p = getPlatform(index);
+        if (p != null) {
+            platforms.set(index, new Platforms(newX, newY, newWidth, newHeight));
+            return true;
+        }
+        return false;
+    }
+
+    // DELETE
+    public boolean deletePlatform(int index) {
+        if (index >= 0 && index < platforms.size()) {
+            platforms.remove(index);
+            return true;
+        }
+        return false;
+    }
+}
