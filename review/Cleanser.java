57cf870708a794870bdd93596dc4ad51ef9aa39c
Quangv2yb <duyquangv2yb@gmail.com>
2025-05-29 08:02:39 +0700

    Add review package

---
+++ review/Cleanser.java
@@ -0,0 +1,13 @@
+package review;
+class Cleanser {
+    private String activeIngredient; //private
+    public void dilute(int percent){
+
+    }
+    public void apply(DirtyThing d){
+
+    }
+    public void scrub(Brush b){
+
+    }
+}
